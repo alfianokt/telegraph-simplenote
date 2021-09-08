6cdea3b2868858b0b22651a3cdfce5ef09680634
Alfian Oktafireza <alfianokt104@gmail.com>
2021-09-08 12:06:46 +0700

    feat: update module to 1.0.5

--- main.js
+++ main.js
@@ -1,17 +1,43 @@
+var __defProp = Object.defineProperty;
+var __markAsModule = (target) => __defProp(target, "__esModule", { value: true });
+var __export = (target, all) => {
+  __markAsModule(target);
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+
 // src/path-parser.ts
+var path_parser_exports = {};
+__export(path_parser_exports, {
+  decode: () => decode,
+  encode: () => encode
+});
 var c = "abcdefghijklmnopqrstuvwxyzABCDEF";
 var ca = c.split("");
+var encode = (p) => {
+  try {
+    const ps = p.split("-");
+    return [
+      ca[parseInt(ps[1])],
+      ca[parseInt(ps[2])],
+      ps.length > 3 ? ps[3].split("").map((e) => ca[e]).join("") : ""
+    ].join("");
+  } catch (e) {
+    return "";
+  }
+};
 var decode = (c2) => {
   try {
     const cs = c2.split("");
     return cs.map((e, i) => {
       const _ = ca.indexOf(e);
-      return i < 2 ? _ < 10 ? `0${_}` : _ : _;
-    }).join("-");
+      return i < 2 ? (_ < 10 ? `0${_}` : _) + "-" : _;
+    }).join("");
   } catch (e) {
     return null;
   }
 };
 
 // main.ts
-console.log(decode("ccff"));
+console.log(path_parser_exports.encode("p-02-02-5765"));
+console.log(path_parser_exports.decode("ccfhgf"));
--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 // import * as pathParser from './src/path-parser';
 import { pathParser } from './src'
 
-console.log(pathParser.decode('ccff'))
+console.log(pathParser.encode("p-02-02-5765"));
+console.log(pathParser.decode("ccfhgf"));
